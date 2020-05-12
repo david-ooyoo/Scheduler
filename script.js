05e5bc83551da334a81fb9cbc215e50bb5b0ec87
David Park <david-ooyoo@users.noreply.github.com>
2020-05-11 21:44:36 -0700

    commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const listContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+
+const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_LIST_KEY_ID_KEY = 'task.selectedListId'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_LIST_KEY_ID_KEY)
+
+newListForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const listName = newListInput.value
+    if (listName == null || listName === '') return
+    const list = createList(listName)
+    newListInput.value = null
+    lists,push(list)
+    render()
+})
+
+function createList(name) {
+    return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+}
+
+function saveandRender() {
+    save()
+    render()
+}
+
+function render() {
+    clearElement(listContainer)
+    lists.forEach(list => {
+        const listElement = document.createElement('li')
+        listElement.dataset.listId = list.id
+        listElement.classList.add("list-name")
+        listElement.innerText = list.name
+        if (list.id === selectedListId) {
+            listElement.classList.add('active-list')
+        }
+        listContainer.appendChild(listElement)
+    })
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+render()
